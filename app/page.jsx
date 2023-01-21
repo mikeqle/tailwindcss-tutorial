320491e51e5d36d5094f4b848c5ffa729d45d3fa
Mike Le <mike.le@live.com>
2023-01-21 18:33:27 +0700

    colors customization

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <h1 className="text-6xl text-vuejs border-green-600 border-4">Hello world!</h1>
+      <h1 className="text-yellow-600 text-6xl">Hello world!</h1>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vuejs: "#49e659"
+      },
+    },
   },
   plugins: [],
 };
