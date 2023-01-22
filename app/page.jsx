161a2c9595131f3569beab9623130dad68e1dc26
Mike Le <mike.le@live.com>
2023-01-23 01:11:26 +0700

    starter files for typography section

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-sm md:text-base lg:text-lg text-vuejs border-green-600 border-4">Hello world!</h1>
-      <h1 className="text-yellow-600 text-6xl">Hello world!</h1>
+      <h1>Title 1</h1>
+      <h2>Title 2</h2>
+      <h3>Title 3</h3>
+      <p>A regular paragraph</p>
+      <p>A description paragraph</p>
+      <p className="note">A little note</p>
     </div>
   );
 }
