22c57f18e83c4fb3f4735a3a208b3953dbe2a182
Mike Le <mike.le@live.com>
2023-01-23 01:10:20 +0700

    playing with config file and globals.css to test out customizations

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-6xl text-vuejs border-green-600 border-4">Hello world!</h1>
+      <h1 className="text-sm md:text-base lg:text-lg text-vuejs border-green-600 border-4">Hello world!</h1>
       <h1 className="text-yellow-600 text-6xl">Hello world!</h1>
     </div>
   );
