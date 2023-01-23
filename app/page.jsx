1bd5ac325497aae42746f5cadbe06fedfc8e56d1
Mike Le <mike.le@live.com>
2023-01-23 11:06:28 +0700

    playing with typography pt 1

--- app/page.jsx
+++ app/page.jsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Title 1</h1>
-      <h2>Title 2</h2>
-      <h3>Title 3</h3>
-      <p>A regular paragraph</p>
-      <p>A description paragraph</p>
-      <p className="note">A little note</p>
+      <h1 className="text-3xl italic underline">Title 1</h1>
+      <h2 className="text-2xl">Title 2</h2>
+      <h3 className="text-xl">Title 3</h3>
+      <p className="text-base">A regular paragraph</p>
+      <p className="text-sm">A description paragraph</p>
+      <p className="note text-xs">A little note</p>
     </div>
   );
 }
